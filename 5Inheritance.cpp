0acb0148567fba05cc98e130a49bec07444ced41
Maloth Aditya <malothaditya006@gmail.com>
2023-08-06 12:20:34 +0530

    Add Inheritance code

--- 5Inheritance.cpp
+++ 5Inheritance.cpp
@@ -1,17 +1,7 @@
-//Abstraction is the process of hiding the implementation details and showing only the functionality to the user.
-//Abstraction is used to achieve security, reusability, extensibility, maintainability, testability, loose coupling, low complexity and high cohesion.
-//Abstraction and encapsulation are not the same.
-//Abstraction is used to hide the implementation details and show only the functionality to the user.
-//Encapsulation is used to hide the data and information.
-
-//Encapsulation is the process of wrapping up of data and information under a single unit.
-//Encapsulation is also known as data hiding.
-//Encapsulation is done using access modifiers.
-//Encapsulation is used to achieve abstraction.
-//Encapsulation is used to make the code more flexible and maintainable.
-//Encapsulation is used to achieve security, reusability, extensibility, maintainability, testability, loose coupling, low complexity and high cohesion.
-//Setter and getter functions are used to access private variables.
-//Setter and getter functions are used to set and get the values of private variables.
+//Inheritance is the process of acquiring the properties of one class by another class.
+//Inheritance is of two types: Single Inheritance and Multiple Inheritance.
+//Single Inheritance is the process of inheriting the properties of one class by another class.
+//Multiple Inheritance is the process of inheriting the properties of more than one class by another class.
 
 #include <iostream>
 using namespace std;
@@ -26,7 +16,6 @@ class AbstractEmployee{ // This is an abstract class
 
 class Employee:AbstractEmployee{ //Employee is a derived class of AbstractEmployee
     private: // All the things below this are private
-        string employeeName;
         string employeeCompany;
         int age;
         int salary;
@@ -39,6 +28,8 @@ class Employee:AbstractEmployee{ //Employee is a derived class of AbstractEmploy
             salary = 0;
             employeeId = 0;
         }
+    protected:
+        string employeeName;
     public: // All the things below this are public
         Employee(string name, string company, int age){ // This is a parameterized constructor
             employeeName = name;
@@ -82,11 +73,35 @@ class Employee:AbstractEmployee{ //Employee is a derived class of AbstractEmploy
         }
 };
 
-int main(){
-    Employee employee1 = Employee("Maloth Aditya", "myCompany", 95); // This is how we call a parameterized constructor.
-    Employee employee2 = Employee("Rohith", "Amazon", 25); // This is how we call a parameterized constructor.   
+class Developer:public Employee{ // Developer is a derived class of Employee
+    public: // All the things below this are public
+        string favProgrammingLanguage;
+        Developer(string name, string company, int age, string favProgrammingLanguage):Employee(name, company, age){ // This is a parameterized constructor
+        //Since the Employee already has a constructor, we need to call that constructor using the Employee(name, company, age) syntax.
+            this->favProgrammingLanguage = favProgrammingLanguage;
+        }
+        void FixBug(){
+            cout << getName() << " fixed bug using " << favProgrammingLanguage << endl;
+        }
+};
 
-    employee1.AskForPromotion();
-    employee2.AskForPromotion();
+class Teacher:public Employee{ // Teacher is a derived class of Employee
+    public: // All the things below this are public
+        string subject;
+        Teacher(string name, string company, int age, string subject):Employee(name, company, age){ // This is a parameterized constructor
+        //Since the Employee already has a constructor, we need to call that constructor using the Employee(name, company, age) syntax.
+            this->subject = subject;
+        }
+        void PrepareLesson(){
+            cout << employeeName << " is preparing " << subject << " lesson." << endl;
+        }
+};
+
+int main(){
+    Developer dev = Developer("Maloth Aditya", "Google", 19, "C++");
+    dev.FixBug();
+    Teacher teacher = Teacher("Jack", "Cool School", 35, "History");
+    teacher.PrepareLesson();
+    teacher.AskForPromotion();
     return 0;
 }
